84b0aef6931c5fd237d0ce3c78a57d4bc481148f
Adriano <adriano_alves_2008@hotmail.com>
2022-11-14 13:58:51 -0300

    adicionando as funcionalidades do resume

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from './components/Form'
 import Header from './components/Header'
 import Resume from './components/Resume'
 import Global from './styles/global'
 
 function App() {
+  const data = localStorage.getItem('transactions')
+
+  //Se existir algum dado no localStorage ele vai agir, se não tiver ele vai devolver um array vazio
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  )
+
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const amountExpense = transactionsList
+      .filter(item => item.expense)
+      .map(transaction => Number(transaction.amount))
+
+    const amountIncome = transactionsList
+      .filter(item => item.expense)
+      .map(transaction => Number(transaction.amount))
+
+    const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2)
+    const income = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2)
+
+    const total = Math.abs(income - expense).toFixed(2)
+
+    setIncome(`R$ ${income}`)
+    setExpense(`R$ ${expense}`)
+    setTotal(`${Number(income) < Number(expense) ? '-' : ''} R$ ${total}`)
+  }, [transactionsList])
   return (
     <>
       <Header />
-      <Resume />
+      <Resume income={income} expense={expense} total={total} />
       <Form />
       <Global />
     </>
